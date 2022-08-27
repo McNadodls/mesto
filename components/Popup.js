b3a14441cb5efb447bec2522bdfb816d66966d9b
McNadodls <103933912+McNadodls@users.noreply.github.com>
2022-08-27 19:43:43 +0300

    V30.4

---
+++ components/Popup.js
@@ -0,0 +1,34 @@
+export default class Popup {
+  constructor(selectorPopup) {
+    this._popup = document.querySelector(selectorPopup);
+    this._handleEscClose = this._handleEscClose.bind(this)
+  }
+
+  _handleEscClose (evt) {
+    if (evt.key === 'Escape') {
+      this.closePopup();
+    };
+  }
+
+  openPopup() {
+    this._popup.classList.add("popup_opened");
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  closePopup() {
+    this._popup.classList.remove("popup_opened");
+    document.removeEventListener('keydown', this._handleEscClose);
+    
+  }
+
+  setEventListeners() {
+    this._popup.addEventListener("mousedown", (evt) => {
+      if (
+        evt.target.classList.contains("button_type_close") ||
+        evt.target.classList.contains("popup_opened")
+      ) {
+        this.closePopup();
+      }
+    });
+  }
+}
