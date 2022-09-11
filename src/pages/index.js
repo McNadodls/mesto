968fef2f78057eb7c9f88d3111f932cd7de6d558
McNadodls <103933912+McNadodls@users.noreply.github.com>
2022-09-11 14:10:26 +0300

    V40.2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import "./pages/index.css";
+import "./index.css";
 import {btnEditProfile, btnAddElement, avatar, configForm} from "../components/utils/constants.js";
 
 import Card from "../components/Card.js";
